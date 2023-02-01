5725f66b870ae1f2d2025382f50ab099771d27f5
giocarlesso <carlessogio@gmail.com>
2023-02-01 20:10:16 -0300

    feat: refactoring and using the alert component.

--- src/pages/TaskForm.vue
+++ src/pages/TaskForm.vue
@@ -1,39 +1,96 @@
 <template>
     <div class="task-form">
-        <h2>{{ $route.params.task_id ? "Edit" : "Create" }}</h2>
-        <input type="date" v-model="date" />
-        <input type="number" step="0.5" min="0.5" max="12" v-model="duration" />
-        <select v-model="activity">
-            <option v-for="activity in activities" :key="activity.id">
-                {{ activity.name }}
-            </option>
-        </select>
-        <textarea v-model="comment" />
+        <div class="task-form-container">
+            <!-- TODO: style these labels and put the required icon -->
+            <h2>{{ $route.params.task_id ? "Edit" : "Create" }} task</h2>
+            <alert-notifications
+                :title="alertData.title"
+                :message="alertData.message"
+                :alertType="alertData.alertType"
+                :showAlert="alertData.showAlert"
+            />
+            <div class="input-group">
+                <label for="">Date</label>
+                <input class="input-form" type="date" v-model="date" required />
+            </div>
+            <div class="input-group">
+                <label for="">Duration</label>
+                <input
+                    class="input-form"
+                    type="number"
+                    step="0.5"
+                    min="0.5"
+                    max="12"
+                    required
+                    v-model="duration"
+                />
+            </div>
+            <div class="input-group">
+                <label for="">Activity</label>
+                <select class="input-form" v-model="activity">
+                    <option v-for="activity in activities" :key="activity.id">
+                        {{ activity.name }}
+                    </option>
+                </select>
+            </div>
+            <div class="input-group">
+                <label for="">Comment</label>
+                <textarea class="input-form" v-model="comment" />
+            </div>
+            <div class="task-form_buttons">
+                <!-- TODO: style these buttons -->
+                <button @click="$router.go(-1)">Cancel</button>
+                <button @click.prevent="onSubmit">
+                    {{ $route.params.task_id ? "Save" : "Create" }} task
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import AlertNotifications from "@/components/AlertNotifications"
+
     export default {
         name: "TaskForm",
+        components: {
+            AlertNotifications,
+        },
 
         data() {
             return {
                 activities: null,
+                tasks: null,
+                errors: [],
                 date: null,
                 duration: null,
                 activity: null,
                 comment: null,
+                alertData: {
+                    showAlert: false,
+                    title: "",
+                    message: "",
+                    alertType: "",
+                },
             }
         },
 
         created() {
             this.fetchActivities()
+
             if (this.$route.params.task_id) {
                 this.fetchTasks()
             }
         },
 
         methods: {
+            updateAlert(title, message, alertType) {
+                this.alertData = { title, message, alertType, showAlert: true }
+
+                setTimeout(() => {
+                    this.alertData.showAlert = false
+                }, 3000)
+            },
             fetchActivities() {
                 this.$http
                     .get("http://localhost:8888/test/getActivities")
@@ -41,31 +98,70 @@
                         this.activities = data
                     })
                     .catch(error => {
-                        //TODO: show error message on the screen
                         //TODO: make a service for the requests?
-                        console.log(error)
+                        this.updateAlert(
+                            "Error",
+                            `Activities failed do load\n ${error.message}`,
+                            "error"
+                        )
                     })
             },
             fetchTasks() {
                 this.$http
                     .get("http://localhost:8888/test/getTasks")
                     .then(({ data }) => {
-                        const task = data.find(
-                            task => task.id === this.$route.params.task_id
-                        )
-                        this.fillForm(task)
+                        this.tasks = data
+                        this.fillForm()
                     })
                     .catch(error => {
-                        //TODO: show error message on the screen
                         //TODO: make a service for the requests?
-                        console.log(error)
+                        this.updateAlert("Error", error.message, "error")
                     })
             },
-            fillForm(task) {
-                this.date = this.convertDate(task.date)
-                this.duration = task.duration
-                this.activity = task.activity.name
-                this.comment = task.comment
+            onSubmit() {
+                if (!this.validateForm()) {
+                    return
+                }
+
+                const payload = {
+                    date: this.date,
+                    duration: this.duration,
+                    activity: this.activities.find(
+                        act => act.name == this.activity
+                    ),
+                    comment: this.comment,
+                }
+
+                const headers = {
+                    "Content-Type": "application/json",
+                }
+
+                this.$http
+                    .post("http://localhost:8888/test/saveTask", payload, {
+                        headers: headers,
+                    })
+                    .then(() => {
+                        this.updateAlert(
+                            "Success",
+                            "Task successfully saved",
+                            "success"
+                        )
+                    })
+                    .catch(error => {
+                        //TODO: make a service for the requests?
+                        this.updateAlert("Error", error.message, "error")
+                    })
+                    .finally(this.$router.go(-1))
+            },
+            fillForm() {
+                this.tasks.find(task => {
+                    if (task.id == this.$route.params.task_id) {
+                        this.date = this.convertDate(task.date)
+                        this.duration = task.duration
+                        this.activity = task.activity.name
+                        this.comment = task.comment
+                    }
+                })
             },
             convertDate(date) {
                 date = new Date(date)
@@ -73,8 +169,79 @@
                 date = new Date(date.getTime() - offset * 60 * 1000)
                 return date.toISOString().split("T")[0]
             },
+            validateForm() {
+                this.errors = []
+
+                if (!this.date) {
+                    this.errors.push("Date is required.")
+                }
+
+                if (!this.duration) {
+                    this.errors.push("Duration is required.")
+                }
+                if (this.duration > 12 || this.duration < 0.5) {
+                    this.errors.push("Duration must be between 0.5 and 12.")
+                }
+                if (
+                    this.duration &&
+                    !this.validateDurationSteps(this.duration)
+                ) {
+                    this.errors.push("Duration must be an increment of 0.5.")
+                }
+                if (!this.activity) this.errors.push("Activity is required.")
+
+                //TODO: unique activity on the day
+                let message = this.errors.join("\n")
+                console.log(message)
+
+                this.updateAlert("Error", this.errors.join("\n"), "error")
+
+                return this.errors.length <= 0
+            },
+            validateDurationSteps(duration) {
+                return duration % 0.5 == 0
+            },
         },
     }
 </script>
 
-<style scoped></style>
+<style scoped>
+    .task-form {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .task-form-container {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .task-form_buttons {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    .input-form {
+        box-sizing: border-box;
+        height: 40px;
+        border: 2px solid black;
+        border-radius: 4px;
+        width: 150px;
+        font-size: 16px;
+        padding: 8px;
+        margin: 5px 0 20px 0;
+    }
+
+    .input-group {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .input-group textarea {
+        height: 200px;
+        width: 300px;
+    }
+</style>
--- src/pages/TaskView.vue
+++ src/pages/TaskView.vue
@@ -1,22 +1,41 @@
 <template>
-    <div v-if="tasks">
-        <tasks-list :tasks="tasks" />
-        <button @click="createTask()">New Task</button>
+    <div class="task-view">
+        <alert-notifications
+            :title="alertData.title"
+            :message="alertData.message"
+            :alertType="alertData.alertType"
+            :showAlert="alertData.showAlert"
+        />
+        <div v-if="tasks">
+            <tasks-list v-if="tasks" :tasks="tasks" />
+        </div>
+        <div v-else>
+            <p>No tasks to show</p>
+            <button @click="createTask()">New Task</button>
+        </div>
     </div>
 </template>
 
 <script>
     import TasksList from "@/components/TasksList.vue"
+    import AlertNotifications from "@/components/AlertNotifications"
 
     export default {
         name: "TaskView",
 
         components: {
             TasksList,
+            AlertNotifications,
         },
         data() {
             return {
                 tasks: null,
+                alertData: {
+                    showAlert: false,
+                    title: "",
+                    message: "",
+                    alertType: "",
+                },
             }
         },
 
@@ -25,16 +44,27 @@
         },
 
         methods: {
+            updateAlert(title, message, alertType) {
+                this.alertData = { title, message, alertType, showAlert: true }
+
+                setTimeout(() => {
+                    this.alertData.showAlert = false
+                }, 3000)
+            },
             fetchTasks() {
                 this.$http
                     .get("http://localhost:8888/test/getTasks")
                     .then(({ data }) => {
                         this.tasks = data
+                        this.updateAlert(
+                            "Success",
+                            "Tasks loaded sucessfully",
+                            "success"
+                        )
                     })
                     .catch(error => {
-                        //TODO: show error message on the screen
                         //TODO: make a service for the requests?
-                        console.log(error)
+                        this.updateAlert("Error", error.message, "error")
                     })
             },
 
