4abf552ba911dd0805f4e1cac8c3b921c8341ac6
giocarlesso <carlessogio@gmail.com>
2023-02-09 19:35:14 -0300

    feat: creating new components

---
+++ src/components/LoadingNotification.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="loading-notification" v-if="isLoading">LOADING</div>
+</template>
+
+<script>
+    export default {
+        name: "LoadingNotification",
+        props: {
+            isLoading: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="top-bar">
+        <div class="top-bar__header">
+            <h2 class="page-title">{{ title }}</h2>
+            <img :src="require('../assets/accelsiors-logo.png')" />
+        </div>
+        <hr class="top-bar__separator" />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .top-bar {
+        width: 100%;
+        margin-top: 25px;
+    }
+
+    .top-bar__header {
+        display: flex;
+        place-content: space-between;
+    }
+
+    .page-title {
+        display: flex;
+        align-self: self-end;
+        color: #185685;
+        font-size: 36px;
+    }
+
+    .top-bar__separator {
+        margin: 25px 0;
+    }
+</style>
